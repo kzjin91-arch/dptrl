ccf370f1fefb56c2688d1b4080594ad90843ddd9
kzjin91-arch <kzjin91@gmail.com>
2026-01-01 18:23:58 +0800

    Add files via upload

---
+++ gym-control/scripts/visualize.sh
@@ -0,0 +1,22 @@
+python plot.py --log_dir logs/qlearn_cartpole --all True
+python plot.py --log_dir logs/qlearn_cartpole --weight 0.1
+python plot.py --log_dir logs/qlearn_cartpole --weight 0.2
+python plot.py --log_dir logs/qlearn_cartpole --weight 0.3
+python plot.py --log_dir logs/qlearn_cartpole --weight 0.4
+python plot.py --log_dir logs/qlearn_cartpole --weight 0.5
+python plot.py --log_dir logs/qlearn_cartpole --weight 0.6
+python plot.py --log_dir logs/qlearn_cartpole --weight 0.7
+python plot.py --log_dir logs/qlearn_cartpole --weight 0.8
+python plot.py --log_dir logs/qlearn_cartpole --weight 0.9
+
+
+python plot.py --log_dir logs/dqn_cartpole --all True
+python plot.py --log_dir logs/dqn_cartpole --weight 0.1
+python plot.py --log_dir logs/dqn_cartpole --weight 0.2
+python plot.py --log_dir logs/dqn_cartpole --weight 0.3
+python plot.py --log_dir logs/dqn_cartpole --weight 0.4
+python plot.py --log_dir logs/dqn_cartpole --weight 0.5
+python plot.py --log_dir logs/dqn_cartpole --weight 0.6
+python plot.py --log_dir logs/dqn_cartpole --weight 0.7
+python plot.py --log_dir logs/dqn_cartpole --weight 0.8
+'
